96d48785050172e3b6750f115bb6e2a12a157f38
pelkosom <barci.peter@gmail.com>
2019-06-10 10:00:53 +0200

    Test feature

--- src/test/java/TestOfSerenity.java
+++ src/test/java/TestOfSerenity.java
@@ -1,5 +1,4 @@
 import net.serenitybdd.junit.runners.SerenityRunner;
-import net.thucydides.core.annotations.Manual;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,32 +6,10 @@ import org.junit.runner.RunWith;
 @RunWith(SerenityRunner.class)
 public class TestOfSerenity {
 
-    int a = 5;
+    private int a = 5;
 
-//    @Manual
     @Test
     public void test1() {
         Assert.assertTrue(a > 3);
-    }
-
-    @Test
-    public void test2() {
-        Assert.assertTrue(a > 6);
-    }
-
-    @Test
-    public void test10() {
-
-    }
-
-    @Test
-    public void test3() {
-
-    }
-
-    @Test
-    public void test4() {
-
-
     }
 }
